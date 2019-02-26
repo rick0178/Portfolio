e5ad98fa7b2e130063ba4f6ad921bdac0d57fabb
Rick de Boer <r.e.j.deboer@student.tudelft.nl>
2019-02-26 04:05:25 +0100

    overlay is done

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuBranding = document.querySelector('.menu-branding');
 const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
 let showMenu = false;
 
